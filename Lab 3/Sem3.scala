144125073147067f53e1a9d8c5a82add00786419
JessicaCVieira <119906775+JessicaCVieira@users.noreply.github.com>
2024-03-04 14:59:42 +0000

    Update Sem3

--- Lab 3/Sem3.scala
+++ Lab 3/Sem3.scala
@@ -1,6 +1,6 @@
 import scala.annotation.tailrec
 
-object Ex1{
+/*object Ex1{
 
     def factorial1(n : Int):Int ={
         n match {
@@ -64,12 +64,63 @@ def listRange(lo: Int, hi: Int): List[Int] = {
     else lo :: listRange(lo+1, hi)
 }
 
-//Ex 3 correto
-def ex3(a: List[Any], b: List[Any]) : List[Any] = {
+//Ex 3 
+// a)
+def ex3(a: List[Int], b: List[Int]) : List[Int] = 
+{
     if(a.length != b.length) Nil
-    a match{
-        
+    def loop(a1: List[Int], b1: List[Int], n: Int) : List[Int] = 
+    { 
+        a1 match{
+            case Nil => Nil
+            case x :: xs => x + b1.head :: loop(xs, b1.tail, n-1)
+        }
     }
-
+    loop(a, b, a.length)
 } 
 
+//mais simples (ñ era preciso o loop)
+def ex3a(a: List[Int], b: List[Int]) : List[Int] = 
+{
+    if(a.length != b.length) Nil
+    else 
+        a match{
+            case Nil => Nil
+            case x :: xs => x + b.head :: ex3a(xs, b.tail)
+        } 
+} 
+
+//b) with loop 
+def zipWith[A, B, C](op: (A, B) => C, a: List[A], b: List[B]) : List[C] = 
+{
+    if(a.length != b.length) Nil
+    def loop(a1: List[A], b1: List[B], n: Int) : List[C] = 
+    { 
+        a1 match{
+            case Nil => Nil
+            case x :: xs => op(x, b1.head) :: loop(xs, b1.tail, n-1)
+        }
+    }
+    loop(a, b, a.length)
+}
+
+//b) without loop
+def zipWith2[A, B, C](op: (A, B) => C, a: List[A], b: List[B]) : List[C] = 
+{
+    if(a.length != b.length) Nil
+    else
+        a match{
+            case Nil => Nil
+            case x :: xs => op(x, b.head) :: zipWith2(op, xs, b.tail)
+        }
+}*/
+
+//c)
+def isSorted[A](lst: List[A], ordered: (A,A) => Boolean): Boolean = 
+{
+    lst match {
+        case Nil => true // the teacher does not especify what to do in this case
+        case List(_) => true // the teacher does not especify what to do in this case
+        case head :: second :: tail => ordered(head,second) && isSorted(tail, ordered)
+    }
+}
