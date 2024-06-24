7a5ce627fc5316cc0c6dca46ea284d2d4a690974
JessicaCVieira <119906775+JessicaCVieira@users.noreply.github.com>
2024-06-24 20:33:33 +0100

    teste2024Fase1

--- Teste 1/Teste2024.scala
+++ Teste 1/Teste2024.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 
 type Board = List[List[Cells.Cell]]
 
@@ -9,12 +10,43 @@ object Cells extends Enumeration {
 
 def indexList(lst:List[Cells.Cell], c:Cells.Cell): List[Int] = 
 {
-    def aux(list: List[Cells.Cell], index: Int, acc: List[Int]){
-        
+    @tailrec
+    def aux(list: List[Cells.Cell], index: Int, acc: List[Int]) : List[Int] = 
+    {
         list match
+        {
             case Nil => acc
             case head :: tail => 
+                if(head == c) aux(tail, index + 1, acc :+ index)
+                else aux(tail, index + 1, acc)
+        }
+            
     }
-    
-    
+
+    aux(lst, 0, Nil)  
+
 }
+
+/* Teste
+
+scala> indexList(List(Cells.Circle,Cells.Empty,Cells.Cross, Cells.Circle),Cells.Circle)
+val res4: List[Int] = List(0, 3)
+
+*/
+
+
+def count(lst1:List[Int], lst2:List[Int]):Int = {
+
+    (lst1 foldRight 0) ((item, c) => 
+        if (lst2.contains(item)) c + 1
+        else c )
+
+}
+
+/* Teste
+
+scala> count(List(2,0),List(0,1,2,3))
+val res6: Int = 2
+
+*/
+
